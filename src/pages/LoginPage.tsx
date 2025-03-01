ee50085b737688b491c4d258bf6552993eac7426
Kajsa Classon <ispyspiders@gmail.com>
2025-03-01 15:38:40 +0100

    funktionalitet för att logga in

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,19 +1,38 @@
 import { SignIn, WarningCircle } from "@phosphor-icons/react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
+    const { login, user } = useAuth();
+    const navigate = useNavigate();
+
+    // kontrollera användare
+    useEffect(() => {
+        if (user) {
+            navigate("/profile");
+        }
+    }, [user])
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setError('');
-    }
+        try {
+            await login({ email, password });
+            navigate("/profile");
+        } catch (error) {
+            setError("Inloggning misslyckades! Kontrollera epost/lösenord.")
+        }
+    };
 
 
 
     return (
+
         <div className="p-8">
             <div className="bg-white rounded-lg p-4 mt-2 mx-auto max-w-80 drop-shadow-sm">
                 <h1 className="text-lg text-center text-blue-deep font-semibold my-4">Logga in</h1>
@@ -21,18 +40,30 @@ const LoginPage = () => {
                     {/* Felmeddelande */}
                     {error && (
                         <div className="bg-red-100 border border-red-500 rounded p-2 my-4 flex text-blue-deep text-sm">
-                            <WarningCircle size={24} className="text-red-500 me-2"/> {error}
+                            <WarningCircle size={24} className="text-red-500 me-2" /> {error}
                         </div>
                     )}
 
                     <div className="flex flex-col mb-4">
                         <label className="text-sm text-dust-deep mb-2" htmlFor="email">Epost</label>
-                        <input type="email" id="email" className="text-md p-2 rounded border border-dust-mid drop-shadow-sm focus:bg-pink-light focus:bg-opacity-50" />
+                        <input
+                            type="email"
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="text-md p-2 rounded border border-dust-mid drop-shadow-sm focus:bg-pink-light focus:bg-opacity-50"
+                        />
                     </div>
 
                     <div className="flex flex-col mb-4">
                         <label className="text-sm text-dust-deep mb-2" htmlFor="password">Lösenord</label>
-                        <input type="password" id="password" className="text-md p-2 rounded border border-dust-mid drop-shadow-sm focus:bg-pink-light focus:bg-opacity-50" />
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="text-md p-2 rounded border border-dust-mid drop-shadow-sm focus:bg-pink-light focus:bg-opacity-50"
+                        />
                     </div>
 
                     <button className="bg-blue-deep text-white ps-8 pe-4 py-2 rounded-lg flex mx-auto my-8  drop-shadow-sm hover:bg-blue-mid" type="submit"><span className="me-2">Logga in</span> <SignIn size={24} /></button>
