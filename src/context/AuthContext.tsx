c00d33802ccc89f3e037199d00466be985ad9e2d
Kajsa Classon <ispyspiders@gmail.com>
2025-03-02 10:09:49 +0100

    Lagt till funktion för validering av token

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, ReactNode } from "react";
+import { createContext, useState, useContext, ReactNode, useEffect } from "react";
 import { User, LoginCredentials, AuthResponse, AuthContextType } from "../types/auth.types";
 
 // skapa context
@@ -11,8 +11,8 @@ interface AuthProviderProps {
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [user, setUser] = useState<User | null>(null);
 
+    // Logga in
     const login = async (credentials: LoginCredentials) => {
-        console.log(credentials);
         try {
             const res = await fetch("http://localhost:8000/api/login", {
                 method: "POST",
@@ -33,6 +33,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
         }
     }
 
+    // Logga ut
     const logout = async () => {
         try {
             const response = await fetch("http://localhost:8000/api/logout", {
@@ -51,6 +52,38 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
             throw error;
         }
     }
+
+    // Validera token
+    const checkToken = async () => {
+        const token = localStorage.getItem("yarnToken");
+        if (!token) {
+            console.log("token saknas");
+            return;
+        }
+
+        try {
+            const res = await fetch("http://localhost:8000/api/user", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token
+                }
+            });
+            if (res.ok) {
+                const data = await res.json();
+                setUser(data);
+            }
+        } catch (error) {
+            console.error("Error in checkToken:", error);
+            localStorage.removeItem("yarnToken");
+            setUser(null);
+        }
+    }
+
+    useEffect(() => {
+        checkToken();
+    }, []);
+
 
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
