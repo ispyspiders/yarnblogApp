62c4ec170927f91246e443141e6e419a2c9b196e
Kajsa Classon <ispyspiders@gmail.com>
2025-03-02 10:09:22 +0100

    justerat huvudmeny. In-/utloggning och anv. ligger separerat från ul. Lagt till ikoner i mobil meny.

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom"
-import { ArrowLeft, List, UserCircle } from "@phosphor-icons/react"
+import { ArrowLeft, Article, HouseLine, List, SignIn, UserCircle, Yarn } from "@phosphor-icons/react"
 import { useState } from "react"
 import { useAuth } from "../context/AuthContext"
 
@@ -18,26 +18,29 @@ const MainNav = () => {
         md:static md:flex-row md:justify-between  ${showMenu ? '' : 'hidden md:flex'}`}>
 
             <ul className={`flex flex-col justify-center p-16 md:flex-row md:justify-start md:p-2 md:ps-12 ${showMenu ? '' : 'hidden md:flex'}`}>
-                <li className="my-2 text-xl hover:text-blue-light md:me-8 md:text-base"><NavLink to="/" onClick={toggleMobileMenu}>Startsida</NavLink></li>
-                <li className="my-2 text-xl hover:text-blue-light md:me-8 md:text-base"><NavLink to="/about" onClick={toggleMobileMenu}>Om sidan</NavLink></li>
-                <li className="my-2 text-xl hover:text-blue-light md:me-8 md:text-base"><NavLink to="/blog" onClick={toggleMobileMenu}>Blogg</NavLink></li>
-                <li className="my-2 text-xl hover:text-blue-light md:me-8 md:text-base"><NavLink to="/profile" onClick={toggleMobileMenu}>Profil</NavLink></li>
+                <li className="my-2 text-xl flex hover:text-blue-light md:me-8 md:text-base"><HouseLine size={24} className="me-2 md:hidden"/><NavLink to="/" onClick={toggleMobileMenu}>Startsida</NavLink></li>
+                <li className="my-2 text-xl flex hover:text-blue-light md:me-8 md:text-base"><Yarn size={24} className="me-2 md:hidden"/><NavLink to="/about" onClick={toggleMobileMenu}>Om sidan</NavLink></li>
+                <li className="my-2 text-xl flex hover:text-blue-light md:me-8 md:text-base"><Article size={24} className="me-2 md:hidden"/><NavLink to="/blog" onClick={toggleMobileMenu}>Blogg</NavLink></li>
+                <li className="my-2 text-xl flex hover:text-blue-light md:me-8 md:text-base"><UserCircle size={24} className="me-2 md:hidden"/><NavLink to="/profile" onClick={toggleMobileMenu}>Profil</NavLink></li>
 
             </ul >
 
             <hr className="mt-4 md:hidden" />
             {
                 !user ?
-                    <NavLink to="/login" onClick={toggleMobileMenu} className="hover:text-blue-light">Logga in</NavLink>
+                    <div className="flex content-center justify-start items-center px-16 text-xl hover:text-blue-light md:text-base">
+                        <SignIn size={24} className="me-2 md:hidden "/>
+                        <NavLink to="/login" onClick={toggleMobileMenu} className="hover:text-blue-light">Logga in</NavLink>
+                    </div>
                     :
                     <div className="flex content-center justify-between items-center px-16 font-bold text-sm">
                         <div className="flex items-center">
-                        <UserCircle size={24} />
-                        <span className="ms-2 me-4">{user.name}</span>
+                            <UserCircle size={24} />
+                            <span className="ms-2 me-4">{user.name}</span>
                         </div>
                         <div className="flex items-center">
-                        <p className="pe-2 text-sm font-light">Inte du?</p> 
-                        <button onClick={logout} className="text-sm hover:text-blue-light">Logga ut</button>
+                            <p className="pe-2 text-sm font-light">Inte du?</p>
+                            <button onClick={logout} className="text-sm hover:text-blue-light">Logga ut</button>
                         </div>
 
                     </div>
