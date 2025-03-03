07051c287b37f7451835a0d7dfba97961ff743c4
Kajsa Classon <ispyspiders@gmail.com>
2025-03-03 13:53:34 +0100

    funktionalitet för att läsa in alla blogginlägg

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,11 +1,71 @@
+import { useEffect, useState } from "react";
+import PostCardSmall from "../components/PostCardSmall"
+import { SpinnerGap, WarningCircle } from "@phosphor-icons/react";
+import { Post } from '../types/post.types'
+
 
 const BlogPage = () => {
-    return (
-      <div>
-        <h1>Blogg</h1>
-        <p>Lisa alla blogginlägg</p>
-      </div>
-    )
+  // states
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  // Hämta inlägg vid initiell rendering
+  useEffect(() => {
+    getPosts();
+  }, [])
+
+  // Hämta inlägg
+  const getPosts = async () => {
+    try {
+      setLoading(true);
+      const resp = await fetch(`http://localhost:8000/api/posts`, {
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        }
+      });
+      if (!resp.ok) {
+        throw Error;
+      }
+      const data = await resp.json();
+      setPosts(data);
+      setError(null);
+    } catch (error) {
+      console.log("Något gick fel vid inläsning av inlägg.");
+      setError("Något gick fel vid inläsningen av inlägg")
+    } finally {
+      setLoading(false);
+    }
   }
-  
-  export default BlogPage
+
+
+  return (
+    <div className="p-4 pt-8 md:p-16">
+      <h1>Blogg</h1>
+      {/* Laddar... */}
+      {loading && <div className="flex items-center col-span-full bg-white p-4 rounded mt-4 drop-shadow-sm text-blue-deep">
+        Läser in inlägg... <SpinnerGap className="animate-spin ms-4" />
+      </div>}
+      {/* Felmeddelande */}
+      {error && <div className="flex items-center bg-red-100 border col-span-full border-red-500 rounded p-2 my-4 flex text-blue-deep text-sm">
+        <WarningCircle size={24} className="text-red-500 me-2" /> {error}
+      </div>
+      }
+
+      {/* Skriv ut inlägg */}
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <PostCardSmall post={post} />
+        ))
+      ) : (
+        <div className="flex items-center bg-white p-4 rounded mt-4 drop-shadow-sm text-blue-deep">
+          Läser in inlägg... <SpinnerGap className="animate-spin ms-4" />
+        </div>
+      )
+      }
+    </div>
+  )
+}
+
+export default BlogPage
