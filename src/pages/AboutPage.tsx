ae40b422f5fbf7548cf3d7782d462ec1bd308c4a
Kajsa Classon <ispyspiders@gmail.com>
2025-03-03 19:24:38 +0100

    Innehåll "Om oss"

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,10 +1,20 @@
 
 const AboutPage = () => {
-    return (
-      <div>
-          <h1>Om sidan</h1>
-      </div>
-    )
-  }
-  
-  export default AboutPage
+  return (
+    <div className="p-4 pt-8 md:p-16">
+      <h1>Om oss</h1>
+      <p className="max-w-screen-xs md:text-xl md:max-w-screen-sm text-blue-deep">Välkommen till Garnbloggare – din go-to plats för allt som har med garn och hantverk att göra! Vi är en passionerad grupp hantverkare och garnälskare som har samlats här för att dela med oss av vår kärlek till garn och kreativt skapande. Oavsett om du är en erfaren stickare, virkar eller precis har börjat din resa inom garnvärlden, så hoppas vi att du hittar inspiration, tips och idéer som gör ditt kreativa hjärta glatt.</p>
+
+      <h2 className="pt-4">Vår passion för garn</h2>
+      <p className="max-w-96">Vi tror på att garn är mer än bara ett material: det är en väg till självuttryck, kreativitet och avkoppling. För oss handlar det inte bara om att skapa plagg eller accessoarer, utan om att njuta av processen och känna glädje i varje ögla och varv. På Garnbloggare vill vi hylla mångfalden av garn – från det mjukaste ullgarn till det mest färgsprakande akrylgarn – och allt däremellan.
+      </p>
+      <h2 className="pt-4">Vårt mål</h2>
+      <p className="max-w-96">Vårt mål är att skapa en gemenskap där alla, oavsett nivå, kan hitta stöd och inspiration. Vi vill vara din plats för idéer, kunskap och inspiration – och framför allt, vi vill att du ska känna dig som hemma här. Garnvärlden är stor, och vi är här för att utforska den tillsammans med dig.
+      </p>
+      <p className="max-w-96">Tack för att du har valt att besöka oss – vi ser fram emot att dela denna kreativa resa med dig!
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
